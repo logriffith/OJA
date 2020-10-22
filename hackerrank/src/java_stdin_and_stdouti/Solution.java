7831016f399230555aecf41f894a053e19fae554
logriffith <logriffith1991@gmail.com>
2020-10-21 22:40:53 -0600

    finished problem

---
+++ hackerrank/src/java_stdin_and_stdouti/Solution.java
@@ -0,0 +1,22 @@
+package java_stdin_and_stdouti;
+
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+    }
+}
+
+//Scanner object named scanner and uses it to read a String 
+//and an int. It then closes the Scanner object because
+//there is no more input to read
