f6052f0398164c3f85970c655ef7dfd8cfa8bae0
logriffith <logriffith1991@gmail.com>
2020-10-29 22:35:37 -0600

    verified environment variables

--- jdbc_example2/src/main/java/com/app/jdbc/JdbcDemoMain.java
+++ jdbc_example2/src/main/java/com/app/jdbc/JdbcDemoMain.java
@@ -9,7 +9,7 @@ public class JdbcDemoMain {
 
 	public static void main(String[] args) {
 		//for testing createEmployee method
-//		Employee employee = new Employee(110, "jedi", "jedimaster@temple.com", 7789840094L, "active", "unemployed");
+//		Employee employee = new Employee(150, "blaah", "blaaah@yahoo.com", 1111111111L, "active", "unemployed");
 		EmployeeDAO employeeDAO = new EmployeeDAOImpl();
 //		try {
 //			if (employeeDAO.createEmployee(employee)>0) {//if there is a positive number of rows inserted
@@ -22,39 +22,37 @@ public class JdbcDemoMain {
 		
 		//testing updateEmployeePhone method
 //		int id=105;
-//		long newPhone=1010101010L;
+//		long newPhone=1010101000L;
 //		try {
 //			if(employeeDAO.updateEmployeePhone(id, newPhone)>0){
-//				System.out.println("The updated phone number of employee with id= "+id+" is "+newPhone);
+//				System.out.println("The updated phone number of employee with ID = "+id+" is "+newPhone);
 //			}
 //		} catch (BusinessException e) {
 //			System.out.println(e.getMessage());
 //		}
 		
 		//testing deleteEmployee method
-		int id = 150;
-		try {
-			if(employeeDAO.getEmployeeById(id) != null) {
-				employeeDAO.deleteEmployee(id);
-//				if (employeeDAO.getEmployeeById(id) == null) {
-//					System.out.println("The employee with ID "+id+" was deleted");
-//				}
-			}
-		} catch (BusinessException e) {
-			System.out.println(e.getMessage());
-		}
-		
-		//testing getEmployeeById method
-//		int id = 102;
+//		int id = 150;
 //		try {
-//			Employee employee = employeeDAO.getEmployeeById(id);
-//			if(employee != null) {
-//				System.out.println("Employee found with ID = "+id+" and the details are...");
-//				System.out.println(employee);
+//			if(employeeDAO.getEmployeeById(id) != null) {
+//				employeeDAO.deleteEmployee(id);
+//				System.out.println("The employee with ID = "+id+" was deleted successfully");	
 //			}
 //		} catch (BusinessException e) {
 //			System.out.println(e.getMessage());
 //		}
+		
+		//testing getEmployeeById method
+		int id = 101;
+		try {
+			Employee employee = employeeDAO.getEmployeeById(id);
+			if(employee != null) {
+				System.out.println("Employee found with ID = "+id+" and the details are...");
+				System.out.println(employee);
+			}
+		} catch (BusinessException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
